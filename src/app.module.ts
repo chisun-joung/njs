828a388747f461c9e3da501898840fae38c5ba42
chisun <csjoung@gmail.com>
2023-03-18 19:12:13 +0900

    movie service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { MovieController } from './movie/movie.controller';
+import { MoviesController } from './movies/movies.controller';
+import { MoviesService } from './movies/movies.service';
 
 @Module({
   imports: [],
-  controllers: [MovieController],
-  providers: [],
+  controllers: [MoviesController],
+  providers: [MoviesService],
 })
 export class AppModule {}
